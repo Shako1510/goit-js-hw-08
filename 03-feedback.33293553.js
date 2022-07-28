375add383f6fde2bb9581962f2b266d23ddf1b4f
Shako1510 <93245492+Shako1510@users.noreply.github.com>
2022-07-28 12:28:46 +0000

    Deploying to gh-pages from @ Shako1510/goit-js-hw-08@77c374835bd7df2425860c9cec44a03f80f073d4 🚀

---
+++ 03-feedback.33293553.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");console.log(e),document.querySelector("input").addEventListener("input",(function(e){console.log("Its inputelement")}))}();
+//# sourceMappingURL=03-feedback.33293553.js.map
