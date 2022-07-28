77c374835bd7df2425860c9cec44a03f80f073d4
Shako Yevhen <schako15101992@gmail.com>
2022-07-28 15:28:11 +0300

    тест пуш 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,4 +3,12 @@
 const ggg = document.querySelector('.feedback-form');
 console.log(ggg);
 
+const inputEll = document.querySelector('input');
+
+inputEll.addEventListener('input', showConsol);
+
+function showConsol(e) {
+    console.log('Its inputelement')
+};
+
 
